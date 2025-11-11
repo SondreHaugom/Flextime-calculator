1a6556f954dde66b44fbe0c6d1f635271764887a
SondreHaugom <sondrehaugom@gmail.com>
2025-11-11 12:00:39 +0100

    Remove unnecessary blank lines in calculate_flex function

--- Flextime_calculator.py
+++ Flextime_calculator.py
@@ -77,9 +77,6 @@ def calculate_flex():
 
 
                     
-
-
-
 # definerer en funksjon for å konvertere desimaler til timer og minutter
 def to_hours_and_minutes(timer_float):
     # deklarerer en variabel for timer og minutter
